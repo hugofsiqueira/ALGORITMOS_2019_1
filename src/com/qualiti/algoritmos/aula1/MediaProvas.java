b6e4ef6f089781a882bee74eb138c57a0fcfd2ce
hugofsiqueira <hugo.fsiqueira@gmail.com>
2019-01-22 01:18:06 -0300

    initial import

---
+++ src/com/qualiti/algoritmos/aula1/MediaProvas.java
@@ -0,0 +1,30 @@
+package com.qualiti.algoritmos.aula1;
+
+public class MediaProvas {
+	
+	public static void main(String args[]) {
+		
+		double nota1 = 7.89;
+		double nota2 = 8.5;
+		
+		double media = (nota1 + nota2)/2;
+		
+		System.out.println("A media do aluno foi: "+media);
+		
+		int x = 3876;
+		
+		
+		
+		
+		/**
+		 * DESAFIO:
+		 * 
+		 * Definir uma variável que contenha um numero inteiro de 4 algarismos e 
+		 * imprimir o algarismo da unidade, dezena, centena e milhar separadamente.
+		 * 
+		 */
+		
+		
+	}
+
+}
---
+++ src/com/qualiti/algoritmos/aula1/PrimeiroPrograma.java
@@ -0,0 +1,67 @@
+package com.qualiti.algoritmos.aula1;
+
+
+/**
+ * 
+ * @author hlfs
+ * @version
+
+ *
+ */
+public class PrimeiroPrograma {
+	
+	/**
+	 * 
+	 * @param args
+	 *
+	 */
+	public static void main(String args[]) {
+		
+		System.out.println("Olá, mundo!!!");
+		
+		char letra = 98;
+		
+		byte b = 2;
+		short s = 30;
+		
+		int a = b + s;
+		
+		System.out.println(a);
+		
+		System.out.println(letra);
+		
+		long numero = 765L;
+		
+		float f = 3.8F;
+		
+		double d = 59.36d;
+		
+		f = (float)d; // cast
+		
+		System.out.println(f);
+		
+		int n = 10000;
+		
+		byte c = (byte)n;
+
+		System.out.println(c);
+		
+		boolean estaChovendo = false;
+		boolean ePar = true;
+		
+		double salario;
+		
+		// Padrao Camel Case
+		
+		double q = 5;
+		int w = 2;
+		
+		double resposta = q/w;
+		System.out.println(resposta);
+		
+		long v = (long)(w + q)/c;
+
+		
+	}
+
+}
